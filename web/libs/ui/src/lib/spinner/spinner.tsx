6824a6a0da804ca61e5f2fd22674d8b5e2128a57
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2025-04-16 19:00:00 +0800

    refactor: simplify spinner component and styles; remove unused elements and improve class handling

--- web/libs/ui/src/lib/spinner/spinner.tsx
+++ web/libs/ui/src/lib/spinner/spinner.tsx
@@ -1,6 +1,6 @@
+import { cn } from "@humansignal/shad/utils";
 import type { CSSProperties } from "react";
 import styles from "./spinner.module.scss";
-import { cn } from "@humansignal/shad/utils";
 
 export type SpinnerProps = {
   className?: string;
@@ -10,19 +10,16 @@ export type SpinnerProps = {
 };
 
 export const Spinner = ({ className, style, size = 32, stopped = false }: SpinnerProps) => {
-  const fullClassName = cn(styles.spinner, className);
-  const bodyClassName = cn(styles.body, stopped ? styles.stopped : "");
-
+  const fullClassName = cn(styles.spinner, stopped && styles.stopped, className);
   const sizeWithUnit = typeof size === "number" ? `${size}px` : size;
 
   return (
-    <div className={fullClassName} style={{ ...(style ?? {}), "--spinner-size": sizeWithUnit }}>
-      <div className={bodyClassName}>
-        <span />
-        <span />
-        <span />
-        <span />
-      </div>
-    </div>
+    <div
+      className={fullClassName}
+      style={{
+        ...(style ?? {}),
+        "--spinner-size": sizeWithUnit,
+      }}
+    />
   );
 };
